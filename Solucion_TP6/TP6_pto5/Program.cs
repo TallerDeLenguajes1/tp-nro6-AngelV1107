bf1f22ac31ca83fb6f040c5ae518df967ea79d92
AngelV1107 <angelleandro@live.com>
2019-05-18 17:57:18 -0300

    Modificado punto 5
    Agregada funcion para validar pagina web

--- Solucion_TP6/TP6_pto5/Program.cs
+++ Solucion_TP6/TP6_pto5/Program.cs
@@ -7,16 +7,24 @@ namespace TP6_pto5
     {
         static void Main(string[] args)
         {
-            string e1, e2, e3;
+            string e1, e2, e3, p1, p2, p3;
 
 
             e1 = "email@ejemplo.com";
             e2 = "email@ejemplo@.com";
             e3 = "email.ejemplo.com";
 
+            p1 = "www.pagina.com";
+            p2 = "wwwpagina.com";
+            p3 = "www.com";
+
             emailValido(e1);
             emailValido(e2);
             emailValido(e3);
+
+            paginaValida(p1);
+            paginaValida(p2);
+            paginaValida(p3);
 
             Console.ReadKey();
         }
@@ -29,11 +37,28 @@ namespace TP6_pto5
 
             if (rgx.IsMatch(email))
             {
-                Console.WriteLine("Es válido");
+                Console.WriteLine("{0} es email válido", email);
             }
             else
             {
-                Console.WriteLine("Es inválido");
+                Console.WriteLine("{0} es email inválido", email);
+            }
+
+        }
+
+        public static void paginaValida(string pagina)
+        {
+            string pattern = @"^www\.\w+\.com";
+
+            Regex rgx = new Regex(pattern);
+
+            if (rgx.IsMatch(pagina))
+            {
+                Console.WriteLine("{0} es pagina válida", pagina);
+            }
+            else
+            {
+                Console.WriteLine("{0} es pagina inválida", pagina);
             }
 
         }
